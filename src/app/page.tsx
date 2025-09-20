2dcb0c8d23fdaabc473b4fbc36807020a17a5955
yashmandi <yashmandi18@gmail.com>
2025-09-21 01:13:22 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,36 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center px-6 py-12 bg-gray-50">
-      <div className="max-w-xl mx-auto space-y-4 text-gray-800">
-        <h1 className="text-2xl font-bold">yash mandi.</h1>
+      <div className="max-w-xl mx-auto space-y-4 text-slate-800">
+        <h1 className="text-3xl font-bold">yash mandi.</h1>
+        <div className="space-x-3 flex mb-7">
+          {/* <a href="mailto:yash.mandi@oneclarity.ai" className="underline hover:no-underline">e-mail</a> */}
+          <a href="https://x.com/yashmandi" target="_blank" className="underline hover:no-underline">x (twitter)</a>
+          <a href="https://github.com/yashmandi" target="_blank" className="underline hover:no-underline">github</a>
+          <a href="https://linkedin.com/in/yashmandi" target="_blank" className="underline hover:no-underline">linkedin</a>
+        </div>
         <p>
-          23, grew up in pune, maharashtra, but originally from a small town in karnataka.
-          studied cs ('24 batch) at a tier-2 college and have been working since 2019 - switching between multiple roles before settling into what excites me most: <span className="italic">building things</span>.
+          i'm 23, grew up in pune, maharashtra, but originally from a small town in karnataka.
+          studied cs at a tier-2 college and have been working since 2019 - switching between multiple roles before settling into what excites me most: <span className="italic">building things</span>.
         </p>
         <p>
-          currently, i serve as the CTO at <a href="https://www.oneclarity.ai" target="_blank" className="text-blue-800 underline">OneClarity</a>, an AI platform, and also run  an <a href="https://x.com/yashmandi" target="_blank" className="text-blue-800 underline">ai agency</a> (freelance).
+          currently, i serve as the CTO & founding engineer at <a href="https://www.oneclarity.ai" target="_blank" className="underline hover:no-underline">OneClarity.AI</a>, an AI-based B2B productivity platform for enterprises.
         </p>
 
         <section>
-          <h2 className="text-lg text-black mb-2">some things about me</h2>
+          <h2 className="text-lg font-semibold text-black mb-2">some things about me</h2>
           <ul className="list-disc list-inside">
             <li>i like to build stuff, and i want to build more.</li>
             <li>i write blogs, mostly about tech, and life sometimes (which i don't publish).</li>
             <li>i like to read & mostly listen to podcasts.</li>
             {/* <li>fitness & consistency are important to me because i believe in taking care of myself.</li> */}
             <li>i listen to a lot of music, almost every genre.</li>
-            <li>i'm bit skeptical about social media and currently only use <a href="https://x.com/yashmandi" target="_blank" className="text-blue-800 underline">twitter</a> (sometimes).</li>
+            <li>i'm bit skeptical about social media and currently only use <a href="https://x.com/yashmandi" target="_blank" className="underline hover:no-underline">twitter</a> (sometimes).</li>
           </ul>
         </section>
 
         <section>
-          <h2 className="text-lg text-black mb-2">some things i believe</h2>
+          <h2 className="text-lg font-semibold text-black mb-2">some things i believe</h2>
           <ul className="list-disc list-inside">
             <li>be a generalist, not a specialist. being average at 3 things &gt; being good at 1 thing.</li>
             <li>take on more challenges, in life particularly. there's no point in competing if you don't want to be the best.</li>
@@ -43,7 +49,7 @@ export default function Home() {
 
         <p>
           i will probably add more to this soon- meanwhile feel free to say hi on my{" "}
-          <a href="mailto:yash.mandi@oneclarity.ai" className="text-blue-800 underline">e-mail</a>
+          <a href="mailto:yash.mandi@oneclarity.ai" className="underline hover:no-underline">e-mail</a>
         </p>
       </div>
     </main>
